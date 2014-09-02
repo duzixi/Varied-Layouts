c94139ed0b98b98d16aec9772528ec1ed5d82cd6
Du Zixi <duzixi@gmail.com>
2014-09-02 19:38:24 +0800

    Create HoneyCombLayout.h

---
+++ HoneyCombLayout.h
@@ -0,0 +1,17 @@
+//
+//  HoneyCombLayout.h
+//  Demo-Layouts
+//
+//  Created by 杜子兮(duzixi) on 14-9-1.
+//  Copyright (c) 2014年 lanou3g.com All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HoneyCombLayout : UICollectionViewLayout
+
+@property (nonatomic, assign) NSInteger margin;
+@property (nonatomic, assign) NSInteger oX;
+@property (nonatomic, assign) NSInteger oY;
+
+@end
